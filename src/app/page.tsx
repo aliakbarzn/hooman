e5c09ae8ea1c728ada2eb963e2a64c6598f869c3
AlirezaBohlool <dev.alirezabohlool@gmail.com>
2024-11-17 14:30:22 +0330

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FindSalon from "@/sections/home/find-salon";
 import Header from "@/sections/home/header";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
   return ( 
     <div className="w-full h-full">
       <Header/>
+      <FindSalon/>
     </div>
    );
 }
--- src/sections/home/header/index.tsx
+++ src/sections/home/header/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import Menu from './menu'
+import React from "react";
+import Menu from "./menu";
+import Image from "next/image";
+import woman from "@/assets/images/header/woman.svg";
+import card1 from "@/assets/images/header/card-1.svg";
+import card2 from "@/assets/images/header/card-2.svg";
+
 
 export default function Header() {
   return (
-    <div className='w-full h-full'>
-        <Menu/>
+    <div className="relative w-full h-full">
+    <Menu />
+    <div className="mt-10 flex flex-col justify-center items-center relative"> 
+      <div>
+        <Image alt="thaimassagegruppen" src={woman} />
+      </div>
+      <div className="absolute top-20 left-[300px] righ-0">
+        <Image alt="card1" src={card1} width={316} height={113} />
+      </div>
+      <div className="absolute top-42 right-64">
+        <Image alt="card2" src={card2} />
+      </div>
     </div>
-  )
+  </div>
+  
+  );
 }
