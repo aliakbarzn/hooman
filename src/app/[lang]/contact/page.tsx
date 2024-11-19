b1457a5d5cd3513d439ed9c52f6aa80007e393ce
aliakbar zabihnejad <aliakbarzb1999@gmail.com>
2024-11-19 10:42:10 +0330

    feat : add contact page

---
+++ src/app/[lang]/contact/page.tsx
@@ -0,0 +1,12 @@
+
+import ContactIndex from '@/sections/contact-page'
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <ContactIndex />
+    </div>
+  )
+}
+
