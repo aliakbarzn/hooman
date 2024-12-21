2023137211c4e30d2abf0243b0f5d33e77f7a4ff
aliakbar zabihnejad <aliakbarzb1999@gmail.com>
2024-12-21 13:25:40 +0330

    feat & fix : removing id

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -6,14 +6,18 @@ import { useTranslations } from 'next-intl'
 import { useState } from 'react'
 
 export default function Dashboard() {
+  const t = useTranslations("Dashboard.dashboard-items")
+  const pathname = usePathname()
 
   const [selectedItem, setSelectedItem] = useState<string | null>(null)
 
-  const t = useTranslations("Dashboard.dashboard-items")
+  useEffect(() => {
+    setSelectedItem(`/${pathname.split('/')[2]}`)
+    !pathname.split('/')[2] && setSelectedItem('/')
+  }, [])
 
   const dashboardItems = [
     {
-      id: 'messages',
       href: '/messages',
       icon: <EnvelopeIcon />,
       title: t('Messages'),
@@ -22,7 +26,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'dashboard',
       href: '/',
       icon: <HomeIcon />,
       title: t('Dashboard'),
@@ -31,8 +34,7 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'buy-ads',
-      href: '/',
+      href: '/b',
       icon: <QuoteDollarIcon />,
       title: t('Buy ads'),
       hasChevron: false,
@@ -40,7 +42,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'my-ads',
       href: '/my-ads',
       icon: <AdIcon />,
       title: t('My ads'),
@@ -49,8 +50,7 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'make-an-ad',
-      href: '/',
+      href: '/make-ad',
       icon: <WrenchIcon />,
       title: t('Make an ad'),
       hasChevron: false,
@@ -58,7 +58,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'user-comments',
       href: '/comments',
       icon: <CommentsIcon />,
       title: t('User comments'),
@@ -67,7 +66,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'my-bookings',
       href: '/my-bookings',
       icon: <InventoryIcon />,
       title: t('MyBookings'),
@@ -76,7 +74,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'history-bookings',
       href: '/bookings-history',
       icon: <InventoryIcon />,
       title: t('History bookings'),
@@ -85,7 +82,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'payment-history',
       href: '/payment-history',
       icon: <CircledDollarIcon />,
       title: t('Payment history'),
@@ -94,7 +90,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'connect-booking',
       href: '/connect-booking',
       icon: <BookingIcon />,
       title: t('Connect to booking program'),
@@ -103,8 +98,7 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'booking-plan',
-      href: '/',
+      href: '/booking-plan',
       icon: <CalenderIcon />,
       title: t('Booking planning'),
       hasChevron: false,
@@ -112,7 +106,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'settings',
       href: '/settings',
       icon: <SettingsIcon />,
       title: t('User account setting.title'),
@@ -125,7 +118,6 @@ export default function Dashboard() {
       ],
     },
     {
-      id: 'security',
       href: '/security',
       icon: <ShieldIcon />,
       title: t('security.title'),
@@ -134,7 +126,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'notifications',
       href: '/notifications',
       icon: <BellIcon />,
       title: t('Notifications'),
@@ -143,7 +134,6 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'support',
       href: '/support',
       icon: <HeadphoneIcon />,
       title: t('Support'),
@@ -152,8 +142,7 @@ export default function Dashboard() {
       subItems: [],
     },
     {
-      id: 'exit',
-      href: '/',
+      href: '/exit',
       icon: <ExitIcon />,
       title: t('Exit'),
       hasChevron: false,
@@ -162,30 +151,30 @@ export default function Dashboard() {
     },
   ];
 
-  const handleItemClick = (id: string) => {
-    setSelectedItem((prev) => (prev === id ? null : id)); // Toggle the selected item
+  const handleItemClick = (href: string) => {
+    setSelectedItem((prev) => (prev === href ? null : href)); // Toggle the selected item
   };
 
   return (
-    <div className="w-[315px] p-2 bg-white border-y border-r border-[#D9D9D9]">
+    <div className="w-[315px] p-2 bg-white border-y border-r border-grayC">
       {dashboardItems.map((item) => (
-        <>
+        <div key={item.href} >
           {item.hasBorder && <div className='h-[1px] w-[282px] bg-grayC ml-3 mb-3'></div>}
-          <div key={item.id} className="mb-2">
+          <div className="mb-2">
             {/* Main Menu Item */}
             <Link href={`/dashboard${item.href}`}
-              className={`flex items-center justify-between h-12 p-4 rounded-lg cursor-pointer transition-all ${selectedItem === item.id ? 'border-l-2 border-primary bg-[#FFF9F8]' : 'text-[#1E1E1E]'}`}
-              onClick={() => handleItemClick(item.id)}>
+              className={`flex items-center justify-between h-12 p-4 rounded-lg cursor-pointer transition-all ${selectedItem === item.href ? 'border-l-2 border-primary bg-[#FFF9F8]' : 'text-blackC'}`}
+              onClick={() => handleItemClick(item.href)}>
               <div className="flex items-center gap-x-3">
-                <span className={`w-4 h-4 ${selectedItem === item.id ? 'text-primary' : 'text-black'}`}>{item.icon}</span>
-                <span className={`${selectedItem === item.id ? 'text-primary' : 'text-black'}`}>{item.title}</span>
+                <span className={`w-4 h-4 ${selectedItem === item.href ? 'text-primary' : 'text-black'}`}>{item.icon}</span>
+                <span className={`${selectedItem === item.href ? 'text-primary' : 'text-black'}`}>{item.title}</span>
               </div>
               {(item.hasChevron) ?
-                <span className={`transition-transform ${selectedItem === item.id ? 'rotate-90 text-primary' : 'text-gray-500'}`}><CircledArrowIcon /></span>
+                <span className={`transition-transform ${selectedItem === item.href ? 'rotate-90 text-primary' : 'text-gray-500'}`}><CircledArrowIcon /></span>
                 : null}
             </Link>
             {/* Sub-Items */}
-            {selectedItem === item.id && item.subItems.length > 0 && (
+            {selectedItem === item.href && item.subItems.length > 0 && (
               <div className=" mt-3">
                 {item.subItems.map((subItem, index) => (
                   <p key={index} className="text-sm font-normal text-[#757575] mb-3 ml-4 hover:text-primary cursor-pointer">
@@ -195,7 +184,7 @@ export default function Dashboard() {
               </div>
             )}
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
