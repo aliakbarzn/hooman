8a9aa64188f245806d63b25d133228d4bccbc317
aliakbar zabihnejad <aliakbarzb1999@gmail.com>
2024-12-22 15:07:53 +0330

    fix : multilingual

--- src/sections/searchPage/banner/Banner.tsx
+++ src/sections/searchPage/banner/Banner.tsx
@@ -4,28 +4,28 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 const Banner = () => {
-  const t = useTranslations();
+  const t = useTranslations('SearchPage');
 
   return (
     <div className="relative w-full">
       <Image className="w-full h-[556px]" src={baner} alt="baner" />
       <div className="absolute top-0 left-0 h-full flex flex-col justify-center ml-16">
         <h4 className="text-[#FEE9E7] text-3xl font-bold">
-          {t("SearchPage.Baner.content.text1")}
+          {t("Banner.content.text1")}
         </h4>
         <div className="flex flex-row my-2">
           <h4 className="text-white text-5xl font-bold">
-            {t("SearchPage.Baner.content.text2")}
+            {t("Banner.content.text2")}
           </h4>
           <h4 className="text-white text-4xl font-bold ml-2">
-            {t("SearchPage.Baner.content.text3")}
+            {t("Banner.content.text3")}
           </h4>
         </div>
         <span className="text-[#FEE9E7] text-3xl">
-          {t("SearchPage.Baner.content.text4")}
+          {t("Banner.content.text4")}
         </span>
         <button className="p-3 text-[#F5F5F5] bg-primary text-2xl mt-7 rounded-lg">
-          {t("SearchPage.Baner.button")}
+          {t("Banner.button")}
         </button>
       </div>
     </div>
